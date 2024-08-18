9ce768b08d2d5accc90997717a1e4d49f0e2c591
vbloher <vbloher7@gmail.com>
2024-08-18 22:34:55 +0300

    update fees

--- scripts/publish-app-record.sh
+++ scripts/publish-app-record.sh
@@ -24,8 +24,8 @@ services:
     rpcEndpoint: '${CERC_REGISTRY_REST_ENDPOINT:-https://laconicd.laconic.com}'
     gqlEndpoint: '${CERC_REGISTRY_GQL_ENDPOINT:-https://laconicd.laconic.com/api}'
     chainId: ${CERC_REGISTRY_CHAIN_ID:-laconic_9000-1}
-    gas: 9950000
-    fees: 25000000alnt
+    gas: 9550000
+    fees: 10000000alnt
 EOF
 
 if [ -z "$CERC_REGISTRY_BOND_ID" ]; then
